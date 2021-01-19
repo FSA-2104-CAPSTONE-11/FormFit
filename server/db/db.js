7cbe1659a6e84292417e0501971d44f07cb537ce
ericpkatz <ericpkatz@gmail.com>
2021-01-19 14:42:20 -0500

    add dialect message to fix hba message

--- server/db/db.js
+++ server/db/db.js
@@ -11,6 +11,15 @@ if(process.env.LOGGING === 'true'){
   delete config.logging
 }
 
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if(process.env.DATABASE_URL){
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
