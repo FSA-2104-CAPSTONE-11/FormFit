a319229299954e2f27ee4ad93c58df096ef028b1
ericpkatz <ericpkatz@gmail.com>
2021-03-22 10:26:50 -0400

    webpack was caching changes because of the naming of AuthForm

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/Auth-Form';
+import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import {me} from './store'
 
