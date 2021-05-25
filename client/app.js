42c6eb0fbb3b17362f57432efcb81daeb3bef211
ericpkatz <ericpkatz@gmail.com>
2021-05-25 19:41:14 -0400

    rename files

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Navbar from './components/Navbar'
-import Routes from './routes'
+import Routes from './Routes'
 
 const App = () => {
   return (
--- client/components/home.spec.js
+++ client/components/home.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { Home } from './home'
+import { Home } from './Home'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
