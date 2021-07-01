b77d486d63e114c209e4fd801eef50847de7a9d6
Sam Hannan <samhannan47@gmail.com>
2021-07-01 18:44:53 -0400

    kinda working video, no movenet tho, no pose logs

--- client/Routes.js
+++ client/Routes.js
@@ -36,7 +36,7 @@ class Routes extends Component {
             <Route path="/signup">
               <AuthForm name="signup" />
             </Route>
-            <Route path="detect" component={Detect} />
+            <Route path="/detect" component={Detect} />
           </Switch>
         )}
       </div>
