731832679a83bb75c1a68c16844ea968a2f05172
Finley Matthews <finleymatthews96@gmail.com>
2021-07-02 11:54:38 -0400

    displaying webcam on computer

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,47 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+  const { username } = props;
+  const [video, setVideo] = useState();
+
+  useEffect(() => {
+    let videoElement = document.querySelector("#videoElement");
+    setVideo(videoElement);
+  });
+
+  function startVideo() {
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (error) {
+          console.log("Something went wrong!", error);
+        });
+    }
+  }
 
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <button onClick={startVideo}>Show my face</button>
+      <video autoPlay={true} id="videoElement"></video>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    username: state.auth.username
-  }
-}
+    username: state.auth.username,
+  };
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
