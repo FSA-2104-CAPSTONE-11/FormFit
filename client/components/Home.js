0bcbd09398563bb1a3d239fdab297b9c646eb2fc
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2021-07-07 12:00:20 -0700

    refactored home comp to remove video

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import NavbarOffset from './NavbarOffset';
 
 /**
  * COMPONENT
  */
-export const Home = (props) => {
-  const { username } = props;
-  const [video, setVideo] = useState();
-
-  useEffect(() => {
-    let videoElement = document.querySelector("#videoElement");
-    setVideo(videoElement);
-  });
-
-  function startVideo() {
-    if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then(function (stream) {
-          video.srcObject = stream;
-        })
-        .catch(function (error) {
-          console.log("Something went wrong!", error);
-        });
-    }
-  }
+ export const Home = () => {
+  const username = useSelector((state) => state.auth.username);
 
   return (
     <div>
       <NavbarOffset />
       <h3>Welcome, {username}</h3>
-      <button onClick={startVideo}>Show my face</button>
-      <video autoPlay={true} id="videoElement"></video>
     </div>
   );
 };
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    username: state.auth.username,
-  };
-};
-
-export default connect(mapState)(Home);
+export default Home;
