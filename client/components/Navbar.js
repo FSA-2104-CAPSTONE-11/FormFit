69e8e72660ad459464b5da11db9792bda5827816
glim2 <80374265+glim2@users.noreply.github.com>
2021-07-07 10:29:41 -0500

    Update client/components/Navbar.js

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -135,27 +135,6 @@ const Navbar = () => {
         </Toolbar>
       </AppBar>
     </div>
-    // <div>
-    //   <h1>FS-App-Template</h1>
-    //   <nav>
-    //     {isLoggedIn ? (
-    //       <div>
-    //         {/* The navbar will show these links after you log in */}
-    //         <Link to="/home">Home</Link>
-    //         <a href="#" onClick={handleClickLogout}>
-    //           Logout
-    //         </a>
-    //       </div>
-    //     ) : (
-    //       <div>
-    //         {/* The navbar will show these links before you log in */}
-    //         <Link to="/login">Login</Link>
-    //         <Link to="/signup">Sign Up</Link>
-    //       </div>
-    //     )}
-    //   </nav>
-    //   <hr />
-    // </div>
   );
 };
 
