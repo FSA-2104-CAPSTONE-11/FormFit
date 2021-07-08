c9fcf29210234dea075b362dd2de552a98e6d1a0
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-07-08 17:46:58 -0400

    Update server/api/user.js

--- server/api/user.js
+++ server/api/user.js
@@ -18,9 +18,8 @@ router.get("/", requireToken, async (req, res, next) => {
 // PUT api/user (protected for logged in user)
 router.put("/", requireToken, async (req, res, next) => {
   try {
-    const user = await User.findByToken(req.headers.authorization);
-    await user.update(req.body);
-    const {id, username, email} = user
+    await req.user.update(req.body);
+    const {id, username, email} = req.user
     res.json({id, username, email});
   } catch (error) {
     next(err);
