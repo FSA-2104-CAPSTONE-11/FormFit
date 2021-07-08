117d98549339b830ef95516116646277f65d5694
Garrick Lim <garrick.k.lim@gmail.com>
2021-07-08 14:31:56 -0500

    Feature: edit user profile

--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,10 @@ import thunkMiddleware from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./auth";
 import history from "./poseHistory";
+import user from "./user";
 
 export default configureStore({
-  reducer: { auth, history },
+  reducer: { auth, history, user },
   middleware: [thunkMiddleware],
   devTools: true,
 });
--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,45 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: {User},
+} = require("../db");
+module.exports = router;
+const requireToken = require("./gatekeeping");
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+//GET api/users/:id (protected for logged in user)
+router.get("/:id", requireToken, async (req, res, next) => {
+  try {
+    const user = await User.findOne({where: {id: req.params.id}})
+    res.json(user)
+  } catch (error) {
+    next(err);
+  }
+});
+
+// PUT api/users/:id (protected for logged in user)
+router.put("/:id", requireToken, async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    await user.update(req.body);
+    res.json(user);
+  } catch (error) {
+    next(err);
+  }
+});
