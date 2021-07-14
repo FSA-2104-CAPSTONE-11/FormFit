796f180ac6ff571ccfc5f1f40660c840e5cd6380
Garrick Lim <garrick.k.lim@gmail.com>
2021-07-14 18:08:31 -0500

    drawer closes on outside click

---
+++ client/components/Drawer.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import Drawer from "@material-ui/core/Drawer";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import HomeIcon from "@material-ui/icons/Home";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Divider from "@material-ui/core/Divider";
+import history from "../history";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+}));
+
+const DrawerComponent = (props) => {
+    const classes = useStyles();
+    const {open, setOpen, handleClickLogout, handleDrawerClose} = props
+
+    const handleMenuClick = (pageURL) => {
+        setOpen(false);
+        history.push(pageURL);
+      };
+
+  return (
+    <Drawer
+    className={classes.drawer}
+    variant="persistent"
+    anchor="left"
+    open={open}
+    classes={{
+      paper: classes.drawerPaper,
+    }}
+  >
+    <div className={classes.drawerHeader}>
+      <IconButton onClick={handleDrawerClose}>
+        <ChevronLeftIcon />
+      </IconButton>
+    </div>
+    <List>
+      <ListItem button onClick={() => handleMenuClick("/home")}>
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Home" />
+      </ListItem>
+      <ListItem button onClick={() => handleMenuClick("/detector")}>
+        <ListItemIcon>
+          <VideocamIcon />
+        </ListItemIcon>
+        <ListItemText primary="Detect Squats Here" />
+      </ListItem>
+      <ListItem button onClick={() => handleMenuClick("/history")}>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Pose History" />
+      </ListItem>
+      <ListItem button onClick={() => handleMenuClick("/profile")}>
+        <ListItemIcon>
+          <AccountCircleIcon />
+        </ListItemIcon>
+        <ListItemText primary="My Profile" />
+      </ListItem>
+      <Divider />
+      <ListItem button onClick={handleClickLogout}>
+        <ListItemIcon>
+          <ExitToAppIcon />
+        </ListItemIcon>
+        <ListItemText primary="Logout" />
+      </ListItem>
+    </List>
+  </Drawer>
+  )
+
+}
+
+export default DrawerComponent;
