e50faa8635d826f32b631fa0d37d6c414d2596fb
Sam Hannan <samhannan47@gmail.com>
2021-07-14 12:05:15 -0400

    changes text occurances of squatter to FormFit

--- client/components/Home.js
+++ client/components/Home.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Squatter
+        FormFit
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
