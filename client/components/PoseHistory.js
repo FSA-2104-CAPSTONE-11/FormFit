45746e44499c6a1e9fba32213c6f514763d869fb
alafiand <79876588+alafiand@users.noreply.github.com>
2021-07-14 07:59:43 -0700

    Update client/components/PoseHistory.js

--- client/components/PoseHistory.js
+++ client/components/PoseHistory.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getHistory } from "../store/poseHistory";
 import NotLoggedIn from "./NotLoggedIn";
