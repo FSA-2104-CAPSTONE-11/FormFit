b99d473bb2b678225d21d9e2a49a09d0deef4b83
Garrick Lim <garrick.k.lim@gmail.com>
2021-07-15 10:21:05 -0500

    Form My Fit

--- client/components/Drawer.js
+++ client/components/Drawer.js
@@ -70,7 +70,7 @@ const DrawerComponent = (props) => {
         <ListItemIcon>
           <VideocamIcon />
         </ListItemIcon>
-        <ListItemText primary="Detect Squats Here" />
+        <ListItemText primary="Form My Fit" />
       </ListItem>
       <ListItem button onClick={() => handleMenuClick("/history")}>
         <ListItemIcon>
