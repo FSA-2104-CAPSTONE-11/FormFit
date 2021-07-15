0ff82bf9f0f79880d41a886e6064b5340578844a
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2021-07-15 09:15:25 -0700

    we can load images now

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "url-loader",
+      },
     ],
   },
 };
