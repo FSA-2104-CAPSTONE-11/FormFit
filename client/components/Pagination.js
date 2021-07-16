1630b5eeaa4d520c041275066d5957a98212845c
Finley Matthews <finleymatthews96@gmail.com>
2021-07-16 11:58:35 -0400

    add pagination

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -1,22 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import MUIPagination from "@material-ui/lab/Pagination";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
 
 const Pagination = ({ totalPoses, posesPerPage, paginate }) => {
-  const classes = useStyles();
-
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPoses / posesPerPage); i++) {
-    console.log("are we looping");
     pageNumbers.push(i);
   }
 
@@ -26,7 +13,7 @@ const Pagination = ({ totalPoses, posesPerPage, paginate }) => {
         {pageNumbers.map((number) => {
           return (
             <span key={number} style={{ color: "white" }}>
-              <a onClick={() => paginate(number)}>{number}</a>
+              <a onClick={() => paginate(number)}>{number} </a>
             </span>
           );
         })}
